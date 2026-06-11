add081b4bb743c406ddde1a32e6c03549513025a
Preeti Choure <preetichoure9960@gmail.com>
2026-06-11 21:41:28 +0530

    Update and rename operators to operators.py

---
+++ basics/operators.py
@@ -0,0 +1,22 @@
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+#arithmetic operators
+print(a + b) 
+print(a - b)  
+print(a * b)  
+print(a / b)  
+print(a ** b)  
+print(a % b) 
+
+#comparison operators
+print(a == b)  
+print(a != b)  
+print(a > b)  
+print(a < b)  
+print(a >= b)  
+print(a <= b) 
+
+#logical operators
+print(a > b and a == ("enter a number"))  
+print(a > b or a == ("enter a number"))  
+print(not a > b)
